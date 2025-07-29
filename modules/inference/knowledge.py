d474e22cdba8d3ea1037936e69f14e7b554a34ea
Le Hoang Lam <lhlam2515@gmail.com>
2025-07-29 22:13:26 +0700

    feat: Refactor knowledge base initialization and percept handling in Wumpus World

--- modules/inference/knowledge.py
+++ modules/inference/knowledge.py
@@ -10,7 +10,7 @@ class KnowledgeBase:
         self.n_wumpus = k
 
         # Initialize the knowledge base with clauses
-        self.clauses: set[Clause] = self._init_axioms()
+        self.clauses: set[Clause] = set(~(wumpus(0, 0) | pit(0, 0)))
 
     def __iter__(self):
         """Iterate over the clauses in the knowledge base."""
@@ -43,53 +43,56 @@ class KnowledgeBase:
                 self.clauses.difference_update(set(~new))
             self.clauses = self.clauses.union({new})
 
-    def tell_breeze(self, i, j, value: bool):
-        """Tell the knowledge base about a Breeze percept."""
-        # 1) Create the Breeze and Pit literals
-        B = breeze(i, j)
-        P_adj = [pit(*p) for p in self._make_adjacent(i, j)]
+    def tell_percept(self, i, j, percepts: dict[str, bool]):
+        """Tell the knowledge base about a percept at (i, j)."""
+        # 1) Create the clauses based on the percepts
+        clauses = self._make_percept_clauses(i, j, percepts)
 
         # 2) Add the clauses to the knowledge base
-        # 2a) If Breeze is true, then at least one adjacent Pit must be true
-        self.tell(~B | Clause(*P_adj))
+        self.tell(*clauses)
 
-        # 2b) If any adjacent Pit is true, then Breeze must be true
-        self.tell(*[~P | B for P in P_adj])
-
-        # 3) Add the observed Breeze value
-        self.tell(Clause(B) if value else Clause(~B))
-
-    def tell_stench(self, i, j, value: bool):
-        """Tell the knowledge base about a Stench percept."""
-        # 1) Create the Stench and Wumpus literals
-        S = stench(i, j)
-        W_adj = [wumpus(*w) for w in self._make_adjacent(i, j)]
-
-        # 2) Add the clauses to the knowledge base
-        # 2a) If Stench is true, then at least one adjacent Wumpus must be true
-        self.tell(~S | Clause(*W_adj))
-
-        # 2b) If any adjacent Wumpus is true, then Stench must be true
-        self.tell(*[~W | S for W in W_adj])
-
-        # 3) Add the observed Stench value
-        self.tell(Clause(S) if value else Clause(~S))
-
-    def _init_axioms(self):
-        """Initialize the axioms for the Wumpus World."""
-        clauses: set[Clause] = set()
-
-        # 1/ Start point is (0, 0) safe <=> no Wumpus or Pit
-        clauses.add(Clause(~wumpus(0, 0)))
-        clauses.add(Clause(~pit(0, 0)))
-
-        # 2/ ???
-
-        return clauses
-
-    def _make_adjacent(self, i, j):
+    def _adjacent(self, i, j):
         """Generate adjacent cells for a given cell (i, j)."""
         for di, dj in [(1, 0), (-1, 0), (0, 1), (0, -1)]:
             ni, nj = i + di, j + dj
             if 0 <= ni < self.size and 0 <= nj < self.size:
                 yield (ni, nj)
+
+    def _make_percept_clauses(self, i, j, percepts: dict[str, bool]):
+        """Generate clauses based on percepts for a given cell (i, j)."""
+        clauses = set()
+
+        for percept, value in percepts.items():
+            if percept == 'breeze':
+                clauses.add(breeze(i, j) if value else ~breeze(i, j))
+                clauses.update(self._breeze_axioms(i, j))
+            elif percept == 'stench':
+                clauses.add(stench(i, j) if value else ~stench(i, j))
+                clauses.update(self._stench_axioms(i, j))
+
+        # No Wumpus and Pit in the same cell
+        clauses.add(~wumpus(i, j) | ~pit(i, j))
+
+        return clauses
+
+    def _breeze_axioms(self, i, j):
+        """Generate Breeze axioms for a given cell (i, j)."""
+        B = breeze(i, j)
+        P_adj = [pit(*p) for p in self._adjacent(i, j)]
+
+        # If Breeze is true, then at least one adjacent Pit must be true
+        yield ~B | Clause(*P_adj)
+
+        # If any adjacent Pit is true, then Breeze must be true
+        yield from (~P | B for P in P_adj)
+
+    def _stench_axioms(self, i, j):
+        """Generate Stench axioms for a given cell (i, j)."""
+        S = stench(i, j)
+        W_adj = [wumpus(*w) for w in self._adjacent(i, j)]
+
+        # If Stench is true, then at least one adjacent Wumpus must be true
+        yield ~S | Clause(*W_adj)
+
+        # If any adjacent Wumpus is true, then Stench must be true
+        yield from (~W | S for W in W_adj)
